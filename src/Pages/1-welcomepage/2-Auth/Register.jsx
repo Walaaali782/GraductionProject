0387e335dd2d500de10f11d3440781a179588552
Walaa Ali <105892469+Walaaali782@users.noreply.github.com>
2025-04-15 01:00:31 +0200

    Add files via upload

---
+++ src/Pages/1-welcomepage/2-Auth/Register.jsx
@@ -0,0 +1,14 @@
+import CloudShape from "../../components/CloudShape";
+
+const Register  = () => {
+    return (
+        <div >
+                 <CloudShape/>
+  
+     
+        </div>
+      );
+    }
+    
+    export default Register;
+    
